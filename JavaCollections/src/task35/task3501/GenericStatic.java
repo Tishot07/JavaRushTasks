e472929497d2ce15eb732973dcff3957cf1188dc
Dudnik Mary <m.dudnik12@gmail.com>
2018-04-27 21:44:48 +0300

    add task35

---
+++ JavaCollections/src/task35/task3501/GenericStatic.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task35.task3501;
+
+public class GenericStatic {
+
+    public static Object someStaticMethod(Object genericObject) {
+        System.out.println(genericObject);
+        return genericObject;
+    }
+}
